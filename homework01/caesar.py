2f78a6d63df3856daa97e7e0540f4bb52ce50f4e
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-10-20 16:52:34 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,82 @@
-import typing as tp
+def cipher_encrypt(plain_text, key):
 
+    encrypted = ""
 
-def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
+    for c in plain_text:
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+        if c.isupper(): 
 
+            c_index = ord(c) - ord('A')
 
-def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
+            c_shifted = (c_index + key) % 26 + ord('A')
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+            c_new = chr(c_shifted)
 
+            encrypted += c_new
 
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+        elif c.islower(): 
+
+            c_index = ord(c) - ord('a') 
+
+            c_shifted = (c_index + key) % 26 + ord('a')
+
+            c_new = chr(c_shifted)
+
+            encrypted += c_new
+
+        elif c.isdigit():
+
+            c_new = (int(c) + key) % 10
+
+            encrypted += str(c_new)
+
+        else:
+
+            encrypted += c
+
+    return encrypted
+
+def cipher_decrypt(ciphertext, key):
+
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.isupper(): 
+
+            c_index = ord(c) - ord('A')
+
+            c_og_pos = (c_index - key) % 26 + ord('A')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.islower(): 
+
+            c_index = ord(c) - ord('a') 
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.isdigit():
+
+            c_og = (int(c) - key) % 10
+
+            decrypted += str(c_og)
+
+        else:
+
+            decrypted += c
+
+    return decrypted
+plain_text = "Hello, Honey!"
+
+ciphertext = cipher_encrypt(plain_text, 4)
+
+print("Plain text message:\n", plain_text)
+
+print("Encrypted ciphertext:\n", ciphertext)
