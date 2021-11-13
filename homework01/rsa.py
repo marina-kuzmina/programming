b21d676bc36a6dc916f51db0eedb2dfeafe0867e
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-11-13 12:11:30 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -37,14 +37,14 @@ def multiplicative_inverse(e, phi):
     >>> multiplicative_inverse(7, 40)
     23
     """
-    
+
     def gcdex(a, b):
         if b == 0:
             return a, 1, 0
         else:
             d, x, y = gcdex(b, a % b)
             return d, y, x - y * (a // b)
-    
+
     d, x, y = gcdex(e, phi)
     return x % phi
 
