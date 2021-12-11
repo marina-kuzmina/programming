add9ba1e422da9fd1e5cf3fe79a095a8eb25ae11
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-12-11 13:28:48 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,4 @@
-def encrypt_caesar(plaintext: str) -> str:
+def encrypt_caesar(plaintext: str, shift: int) -> str:
     """
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
@@ -8,17 +8,16 @@ def encrypt_caesar(plaintext: str) -> str:
     ''
     """
     ciphertext = ""
-
-        for sumb in plaintext:
-        if 'A' <= sumb <= 'Z':
+    for sumb in plaintext:
+        if "A" <= sumb <= "Z":
             code_sumb = ord(sumb) + shift
-            if ord('A') > code_sumb or code_sumb > ord('Z'):
+            if ord("A") > code_sumb or code_sumb > ord("Z"):
                 code_sumb -= 26
             ciphertext += chr(code_sumb)
 
-        elif 'a' <= sumb <= 'z':
+        elif "a" <= sumb <= "z":
             code_sumb = ord(sumb) + shift
-            if ord('a') > code_sumb or code_sumb > ord('z'):
+            if ord("a") > code_sumb or code_sumb > ord("z"):
                 code_sumb -= 26
             ciphertext += chr(code_sumb)
         else:
@@ -27,7 +26,7 @@ def encrypt_caesar(plaintext: str) -> str:
     return ciphertext
 
 
-def decrypt_caesar(ciphertext: str) -> str:
+def decrypt_caesar(ciphertext: str, shift: int) -> str:
     """
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
@@ -37,19 +36,23 @@ def decrypt_caesar(ciphertext: str) -> str:
     ''
     """
     plaintext = ""
-        for sumb in ciphertext:
-        if 'A' <= sumb <= 'Z':
+    for sumb in ciphertext:
+        if "A" <= sumb <= "Z":
             code_sumb = ord(sumb) - shift
-            if code_sumb > ord('Z') or code_sumb < ord('A'):
+            if code_sumb > ord("Z") or code_sumb < ord("A"):
                 code_sumb += 26
             plaintext += chr(code_sumb)
 
-        elif 'a' <= sumb <= 'z':
+        elif "a" <= sumb <= "z":
             code_sumb = ord(sumb) - shift
-            if code_sumb > ord('z') or code_sumb < ord('a'):
+            if code_sumb > ord("z") or code_sumb < ord("a"):
                 code_sumb += 26
             plaintext += chr(code_sumb)
         else:
             plaintext += sumb
 
     return plaintext
+
+
+if __name__ == "__main__":
+    print(encrypt_caesar("python", 0))
