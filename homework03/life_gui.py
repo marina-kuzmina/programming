6f43bd9953aefee40f78a670bad8c2008b898317
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-12-22 20:31:53 +0300

    Update life_gui.py

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -10,12 +10,51 @@ class GUI(UI):
 
     def draw_lines(self) -> None:
         # Copy from previous assignment
-        pass
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                             (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                             (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
         # Copy from previous assignment
-        pass
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                if self.life.curr_generation[i][j]:
+                    color = "green"
+                else:
+                    color = "white"
+                pygame.draw.rect(self.screen, pygame.Color(color),
+                                 (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size))
 
     def run(self) -> None:
         # Copy from previous assignment
-        pass
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color('white'))
+        running = True
+        pause = False
+        while running:
+            if not pause:
+                running = self.life.step()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        pause = not pause
+                if event.type == MOUSEBUTTONUP:
+                    mouse_pos = event.pos
+                    x, y = mouse_pos
+                    self.life.curr_generation[y // self.cell_size][x // self.cell_size] = 1
+                    self.draw_grid()
+                    pygame.display.flip()
+                if event.type == pygame.QUIT:
+                    running = False
+            self.draw_grid()
+            self.draw_lines()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+        
+        
