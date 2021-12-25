f678877a576afd5b5d3a65e1a9f389978d156649
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-12-25 13:13:01 +0300

    Update test_refs.py

--- homework04/tests/test_refs.py
+++ homework04/tests/test_refs.py
@@ -2,7 +2,6 @@ import unittest
 
 import pyvcs
 from pyfakefs.fake_filesystem_unittest import TestCase
-
 from pyvcs.refs import get_ref, is_detached, ref_resolve, resolve_head, update_ref
 from pyvcs.repo import repo_create
 
