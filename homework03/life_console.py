1e6602311414c6c96b00bcd80c996b5664ec0ac8
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-12-30 14:32:32 +0300

    Update life_console.py

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -5,23 +5,22 @@ from ui import UI
 
 
 class Console(UI):
-
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
 
     @staticmethod
     def draw_borders(screen) -> None:
-        """ Отобразить рамку. """
-        screen.border('|', '|', '-', '-', '+', '+', '+', '+')
+       """Отобразить рамку."""
+       screen.border("|", "|", "-", "-", "+", "+", "+", "+")
 
     def draw_grid(self, screen) -> None:
-        """ Отобразить состояние клеток. """
+        """Отобразить состояние клеток."""
         for i in range(len(self.life.curr_generation)):
             for j in range(len(self.life.curr_generation)):
                 if self.life.curr_generation[i][j]:
-                    screen.addstr(j + 1, i + 1, '*')
+                    screen.addstr(j + 1, i + 1, "*")
                 else:
-                    screen.addstr(j + 1, i + 1, ' ')
+                    screen.addstr(j + 1, i + 1, " ")
 
     def run(self) -> None:
         screen = curses.initscr()
