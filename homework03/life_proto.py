010fd42f15efe42700d931022f5ecc2acc4b5b4b
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-12-30 15:05:40 +0300

    Update life_proto.py

--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -41,8 +41,8 @@ class GameOfLife:
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))  
-    
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
+            
     def run(self) -> None:
         pygame.init()
         clock = pygame.time.Clock()
