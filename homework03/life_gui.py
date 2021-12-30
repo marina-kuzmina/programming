b0b9e10879a793358fd54dbf0625cd13cdea1e71
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2021-12-30 14:44:39 +0300

    Update life_gui.py

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -7,12 +7,12 @@ from ui import UI
 class GUI(UI):
 
     def __init__(
-            self,
-            life: GameOfLife,
-            cell_size: int = 10,
-            speed: int = 10,
-            width: int = 640,
-            height: int = 480
+        self,
+        life: GameOfLife,
+        cell_size: int = 10,
+        speed: int = 10
+        width: int = 640,
+        height: int = 480
     ) -> None:
         super().__init__(life)
         self.width = width
@@ -33,11 +33,9 @@ class GUI(UI):
 
     def draw_lines(self) -> None:
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (x, 0), (x, self.height))
+            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                             (0, y), (self.width, y))
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
         for i in range(self.cell_height):
