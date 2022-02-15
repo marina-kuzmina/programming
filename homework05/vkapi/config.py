652e40a536b313bcd018f29d286cd51cd3d989ce
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2022-02-15 15:24:15 +0300

    Update config.py

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "dc7bc0afdc7bc0afdc7bc0af5cdc008d93ddc7bdc7bc0afbe705adc7608eca85d4610d6",
     "version": "5.126",
 }
