07c3a81045173e97b983c4d6227a10ac4203e869
marina-kuzmina <90199314+marina-kuzmina@users.noreply.github.com>
2022-03-22 15:58:19 +0300

    Update age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -2,28 +2,32 @@ import datetime as dt
 import statistics
 import typing as tp
 
-from dateutil.relativedelta import relativedelta
 from vkapi.friends import get_friends
 
 
 def age_predict(user_id: int) -> tp.Optional[float]:
     """
     Наивный прогноз возраста пользователя по возрасту его друзей.
-
     Возраст считается как медиана среди возраста всех друзей пользователя
-
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    ages = []
-    friends = get_friends(user_id, fields=["bdate"])
-    for friend in friends.items:
+
+    time = dt.date.today()
+    age_of_people = []
+    list_of_friends = get_friends(user_id, fields=["bdate"]).items
+    for friend in list_of_friends:
         try:
-            birthdate = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")  # type: ignore
-            age = relativedelta(dt.datetime.now(), birthdate).years
-            ages.append(age)
-        except (KeyError, ValueError):
-            pass
-    if not ages:
+            bdate = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")  # type: ignore
+        except:
+            continue
+        age_of_people.append(
+            time.year
+            - bdate.year
+            - (time.month < bdate.month or (time.month == bdate.month and time.day < bdate.day))
+        )
+
+    if age_of_people:
+        return statistics.median(age_of_people)
+    else:
         return None
-    return statistics.median(ages)
