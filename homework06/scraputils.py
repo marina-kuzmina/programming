14cb17f50beca0055de6a3ade98a39cc2b2cec4f
Ḿ <90199314+marina-kuzmina@users.noreply.github.com>
2022-05-24 15:26:52 +0300

    Update scraputils.py

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -13,35 +13,31 @@ def extract_news(parser: BeautifulSoup) -> List[Dict]:
     """
     news_list = []
 
-    titles = parser.findAll('tr', attrs={'class': 'athing'})
-    subtext = parser.findAll('td', attrs={'class': 'subtext'})
+    titles = parser.findAll("tr", attrs={"class": "athing"})
+    subtext = parser.findAll("td", attrs={"class": "subtext"})
 
     for i in range(len(titles)):
-        a = titles[i].findAll('td', attrs={'class': 'title'})[1].find('a')
+        a = titles[i].findAll("td", attrs={"class": "title"})[1].find("a")
         title = a.get_text()
-        url = a['href']
+        url = a["href"]
 
-        author = subtext[i].find('a', attrs={'class': 'hnuser'})
+        author = subtext[i].find("a", attrs={"class": "hnuser"})
         if author:
             author = author.get_text()
 
-        comments = subtext[i].findAll('a')[-1].get_text()
-        if 'comments' in comments:
+        comments = subtext[i].findAll("a")[-1].get_text()
+        if "comments" in comments:
             comments = comments.split("\xa0")[0]
         else:
             comments = 0
 
-        points = subtext[i].find('span', attrs={'class': 'score'})
+        points = subtext[i].find("span", attrs={"class": "score"})
         if points:
             points = points.get_text()
 
-        news_list.append({
-            'author': author,
-            'comments': comments,
-            'points': points,
-            'title': title,
-            'url': url
-        })
+        news_list.append(
+            {"author": author, "comments": comments, "points": points, "title": title, "url": url}
+        )
 
     return news_list
 
@@ -52,8 +48,8 @@ def extract_next_page(parser: BeautifulSoup) -> str:
     :param parser: BeautifulSoup web page object
     :return: next page URL or empty string if it isn't exist
     """
-    more_link = parser.find('a', attrs={'class': 'morelink'})
-    return "" if not more_link else more_link['href']
+    more_link = parser.find("a", attrs={"class": "morelink"})
+    return "" if not more_link else more_link["href"]
 
 
 def get_news(url: str, n_pages: int = 1) -> List[Dict]:
